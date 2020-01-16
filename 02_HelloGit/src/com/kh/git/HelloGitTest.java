fbcda5b6ffe4de9eec155b39064c2b75d3dffc38
user2 <user2@user2-PC>
2020-01-16 19:36:23 +0900

    remote repo 연결후 첫 commit & push!

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -5,6 +5,7 @@ public class HelloGitTest {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요!");
 		System.out.println("저녁 맛있게 드셨어요?");
+		System.out.println("축하드립니다. commit & push!!!");
 	}
 
 }
